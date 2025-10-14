ff89c7df0d9a3e1bd27b1d14968b2af42a2bcb14
p-kryuchkov <fractalgeneral@gmail.com>
2025-10-14 16:07:54 +0300

    Перенес код из урока Поправил использование Connection во всех методах на transaction

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/service/SpendDbClient.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/service/SpendDbClient.java
@@ -1,8 +1,6 @@
 package guru.qa.niffler.service;
 
 import guru.qa.niffler.config.Config;
-import guru.qa.niffler.data.dao.CategoryDao;
-import guru.qa.niffler.data.dao.SpendDao;
 import guru.qa.niffler.data.dao.impl.CategoryDaoJdbc;
 import guru.qa.niffler.data.dao.impl.SpendDaoJdbc;
 import guru.qa.niffler.data.entity.spend.CategoryEntity;
@@ -14,63 +12,59 @@ import java.util.NoSuchElementException;
 import java.util.Optional;
 import java.util.UUID;
 
+import static guru.qa.niffler.data.Databases.transaction;
+
 public class SpendDbClient implements SpendClient {
 
     private static final Config CFG = Config.getInstance();
 
-    private final SpendDao spendDao = new SpendDaoJdbc();
-    private final CategoryDao categoryDao = new CategoryDaoJdbc();
-
     @Override
     public SpendJson createSpend(SpendJson spend) {
-        SpendEntity spendEntity = SpendEntity.fromJson(spend);
-        if (spendEntity.getCategory().getId() == null) {
-            CategoryEntity categoryEntity = categoryDao.create(spendEntity.getCategory());
-            spendEntity.setCategory(categoryEntity);
-        }
-        return SpendJson.fromEntity(
-                spendDao.create(spendEntity)
-        );
+        return transaction(connection -> {
+            SpendEntity spendEntity = SpendEntity.fromJson(spend);
+            if (spendEntity.getCategory().getId() == null) {
+                CategoryEntity categoryEntity = new CategoryDaoJdbc(connection).create(spendEntity.getCategory());
+                spendEntity.setCategory(categoryEntity);
+            }
+            return SpendJson.fromEntity(new SpendDaoJdbc(connection).create(spendEntity));
+        }, CFG.spendJdbcUrl());
     }
 
     @Override
     public CategoryJson createCategory(CategoryJson category) {
-        return CategoryJson.fromEntity(
-                categoryDao.create(
-                        CategoryEntity.fromJson(category)
-                )
-        );
+        return transaction(connection -> {
+            return CategoryJson.fromEntity(new CategoryDaoJdbc(connection).create(CategoryEntity.fromJson(category)));
+        }, CFG.spendJdbcUrl());
     }
 
     @Override
     public CategoryJson updateCategory(CategoryJson category) {
-        return CategoryJson.fromEntity(
-                categoryDao.update(
-                        CategoryEntity.fromJson(category)
-                )
-        );
+        return transaction(connection -> {
+            return CategoryJson.fromEntity(new CategoryDaoJdbc(connection).update(CategoryEntity.fromJson(category)));
+        }, CFG.spendJdbcUrl());
     }
 
     @Override
     public Optional<CategoryJson> findCategoryByNameAndUsername(String categoryName, String username) {
-        return categoryDao.findCategoryByUsernameAndCategoryName(username, categoryName)
-                .map(entity -> CategoryJson.fromEntity(entity));
+        return transaction(connection -> {
+            return new CategoryDaoJdbc(connection).findCategoryByUsernameAndCategoryName(username, categoryName).map(entity -> CategoryJson.fromEntity(entity));
+        }, CFG.spendJdbcUrl());
     }
 
     public Optional<CategoryJson> findCategoryById(UUID id) {
-        return categoryDao.findCategoryById(id)
-                .map(entity -> CategoryJson.fromEntity(entity));
+        return transaction(connection -> {
+            return new CategoryDaoJdbc(connection).findCategoryById(id).map(entity -> CategoryJson.fromEntity(entity));
 
+        }, CFG.spendJdbcUrl());
     }
 
     private SpendEntity enrichSpend(SpendEntity spend) {
-        if (spend.getCategory() != null && spend.getCategory().getId() != null) {
-            CategoryEntity category = categoryDao.findCategoryById(spend.getCategory().getId())
-                    .orElseThrow(() -> new NoSuchElementException(
-                            "Category not found by id: " + spend.getCategory().getId()
-                    ));
-            spend.setCategory(category);
-        }
-        return spend;
+        return transaction(connection -> {
+            if (spend.getCategory() != null && spend.getCategory().getId() != null) {
+                CategoryEntity category = new CategoryDaoJdbc(connection).findCategoryById(spend.getCategory().getId()).orElseThrow(() -> new NoSuchElementException("Category not found by id: " + spend.getCategory().getId()));
+                spend.setCategory(category);
+            }
+            return spend;
+        }, CFG.spendJdbcUrl());
     }
 }
