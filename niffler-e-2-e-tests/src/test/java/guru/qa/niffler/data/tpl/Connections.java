6394817311e3b8617f85dba02a7c2ce9d6998709
p-kryuchkov <fractalgeneral@gmail.com>
2025-10-20 16:58:23 +0300

    Перенес код из урока

---
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/data/tpl/Connections.java
@@ -0,0 +1,29 @@
+package guru.qa.niffler.data.tpl;
+
+public class Connections {
+    private Connections() {
+    }
+
+    private static final Map<String, JdbcConnectionHolder> holders = new ConcurrentHashMap<>();
+
+    public static JdbcConnectionHolder holder(String jdbcUrl) {
+        return holders.computeIfAbsent(
+                jdbcUrl,
+                key -> new JdbcConnectionHolder(
+                        DataSources.dataSource(jdbcUrl)
+                )
+        );
+    }
+
+    public static JdbcConnectionHolders holders(String... jdbcUrl) {
+        List<JdbcConnectionHolder> result = new ArrayList<>();
+        for (String url : jdbcUrl) {
+            result.add(holder(url));
+        }
+        return new JdbcConnectionHolders(result);
+    }
+
+    public static void closeAllConnections() {
+        holders.values().forEach(JdbcConnectionHolder::closeAllConnections);
+    }
+}
