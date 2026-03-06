b97e978f51c08519061ce936d69be5c83c6e1853
Pavel Kryuchkov <krutestkru@gmail.com>
2026-03-06 19:22:40 +0300

    Пытаюсь запустить в докере

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/jupiter/extension/BrowserExtension.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/jupiter/extension/BrowserExtension.java
@@ -1,6 +1,7 @@
 
 package guru.qa.niffler.jupiter.extension;
 
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.WebDriverRunner;
 import com.codeborne.selenide.logevents.SelenideLogger;
@@ -9,6 +10,7 @@ import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.extension.*;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.io.ByteArrayInputStream;
 
@@ -17,6 +19,17 @@ public class BrowserExtension implements
         AfterEachCallback,
         TestExecutionExceptionHandler,
         LifecycleMethodExecutionExceptionHandler {
+
+    static {
+        Configuration.browser = "chrome";
+        Configuration.timeout = 8000;
+        Configuration.pageLoadStrategy = "eager";
+        if ("docker".equals(System.getProperty("test.env"))) {
+            Configuration.remote = "http://selenoid:4444/wd/hub";
+            Configuration.browserVersion = "127.0";
+            Configuration.browserCapabilities = new ChromeOptions().addArguments("--no--sandbox");
+        } 
+    }
 
     @Override
     public void afterEach(ExtensionContext context) throws Exception {
